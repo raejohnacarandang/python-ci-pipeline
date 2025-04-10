750ab1902687a012f1a89337bead742da5acbf3c
raejohnacarandang <raejohncarandang121303@gmail.com>
2025-04-10 11:40:24 +0800

    test

--- test_app.py
+++ test_app.py
@@ -1,10 +1,8 @@
 import pytest
 from app import add, subtract
 
-
 def test_add():
     assert add(2, 3) == 5
 
-
-def test_subract():
-    assert add(2, 3) == 2
+def test_subtract():
+    assert subtract(5, 3) == 2
